9fd11957a15f9239dad41ae6293be7b6d4c0e3ee
vcokltfre <vcokltfre@gmail.com>
2021-04-12 10:26:27 +0100

    fix: censor loggers

--- main.py
+++ main.py
@@ -16,6 +16,6 @@ bot = Bot(
 
 bot.cfg = config
 
-bot.load_initial_cogs("cogs.amp", "cogs.autopin", "cogs.links", "cogs.alerts")
+bot.load_initial_cogs("cogs.amp", "cogs.autopin", "cogs.links", "cogs.alerts", "cogs.antiloggers")
 
 bot.run(botconf["token"])
