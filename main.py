13f5439e1e3b742c360a1ef84556d44996837e49
vcokltfre <vcokltfre@gmail.com>
2021-05-05 22:18:00 +0100

    fix: remove antiloggers

--- main.py
+++ main.py
@@ -16,6 +16,6 @@ bot = Bot(
 
 bot.cfg = config
 
-bot.load_initial_cogs("cogs.amp", "cogs.autopin", "cogs.links", "cogs.alerts", "cogs.antiloggers", "cogs.issues")
+bot.load_initial_cogs("cogs.amp", "cogs.autopin", "cogs.links", "cogs.alerts", "cogs.issues")
 
 bot.run(botconf["token"])
